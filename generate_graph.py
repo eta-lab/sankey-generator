a20f720a9713ef320a6e1397c45153da50d2e2ba
claudedb <UW00ut43>
2020-07-24 17:50:47 -0400

    ETA DB attempt

--- generate_graph.py
+++ generate_graph.py
@@ -5,30 +5,31 @@ from datetime import timedelta
 
 
 def generate_sankey(influx_client, start_date, end_date, building_list, metric_list):
-    # Initializing dataframe
-    df_all_buildings = pd.DataFrame(index=metric_list)
 
-    # Creating a string to extract data from all building
-
-    string_building = ''
-    for element in building_list:
-        string_building = string_building + '"' + element + '"' + ', '
-    string_building = string_building[:-2]
 
     #  include the data from the end date even if the day is not over
     end_date_query = (dt.strptime(end_date[:10],  "%Y-%m-%d") + timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%S')
 
     where_params = {'t0': (start_date+"Z"),
                     't1': (end_date_query+"Z")}
+    # Initializing dataframe
+    df_all_buildings = pd.DataFrame()
+    for metric in metric_list:
+        if metric == "elec_power":
+            query_influx = 'select max(*) from {} where time<=$t0 and time<=$t1'.format(metric, metric)
+            metric_string = "sum_"
+        else:
+            query_influx = 'select sum(*) from {} where time<=$t0 and time<=$t1'.format(metric, metric)
+            metric_string = "sum_"
 
-    query_influx = 'select sum(*) from {} where time >= $t0 and time < $t1'.format(string_building)
+        query_result = influx_client.query(query_influx, bind_params=where_params)
+        query_result[metric].columns = [col_name.replace(metric_string, '')
+                                        for col_name in query_result[metric].columns]
 
-    query_result = influx_client.query(query_influx, bind_params=where_params)
+        df_all_buildings = df_all_buildings.append(query_result[metric])
+    df_all_buildings.index = metric_list
 
-    for element in query_result.keys():
-        query_result[element].columns = [col_name.replace('sum_', '')
-                                         for col_name in query_result[element].columns]
-        df_all_buildings[element] = query_result[element].iloc[0]
+    df_all_buildings = df_all_buildings.loc[:, df_all_buildings.columns.isin(building_list)]
 
     labels, sources, targets, values = generate_sankey_elements(df_all_buildings)
 
